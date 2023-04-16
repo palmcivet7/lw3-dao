e634e97e7bbfdc4da430d3e63cd74aa01e6b4c13
palmcivet7 <palmcivet@proton.me>
2023-04-16 13:03:24 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LW3 DAO
+
+This lesson was made following along with a LW3 lesson. It allows NFT holders of the previous LW3 NFT lesson to participate in a DAO.
---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x0Bd1F6674Aa03ffbdbe58140C0bB00Da5921bCcb";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
